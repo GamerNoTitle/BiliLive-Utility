a6a2888e39d74bd82591cf9e2554681cf3ddacf0
GamerNoTitle <GamerNoTitle@outlook.com>
2025-05-16 00:20:43 +0800

    feat: 使用 flet build 进行打包

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 uv run nuitka --standalone --assume-yes-for-downloads --onefile --windows-icon-from-ico=icon.ico .\console.py
-uv run nuitka --standalone --assume-yes-for-downloads --onefile --windows-icon-from-ico=icon.ico .\gui.py
+uv run flet build windows gui.py --output=gui.build
