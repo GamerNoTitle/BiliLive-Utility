f3a80b64a7a97b77d2184af1aa5c42739fb416b3
GamerNoTitle <GamerNoTitle@outlook.com>
2025-05-16 10:21:49 +0800

    feat: 添加了更多的程序信息

--- build.ps1
+++ build.ps1
@@ -14,7 +14,7 @@ $pwd = Get-Location
 # 构建 GUI 版本
 Write-Host "Building GUI with Flet..."
 # 尝试使用位置参数指定入口文件
-uv run flet build windows > flet_build.log 2>&1
+uv run flet build windows --project "Bililive-Credential-Grabber" --product "B站快速开播及推流码获取工具" --company GamerNoTitle --copyright "Copyright (C) 2025 GamerNoTitle" --description "Bililibe-Credential-Grabber is a tool that helps you turn on livestream without using Livehime and get the credential for livestream which is valid in OBS Studio." --cleanup-app --cleanup-packages > flet_build.log 2>&1
 if ($LASTEXITCODE -eq 0) {
     Write-Host "GUI build successful! Output in gui.build folder."
 } else {
@@ -25,7 +25,7 @@ if ($LASTEXITCODE -eq 0) {
 
 # 构建控制台版本
 Write-Host "Building console executable with Nuitka..."
-uv run nuitka --standalone --assume-yes-for-downloads --onefile --windows-icon-from-ico=icon.ico .\console.py
+uv run nuitka --standalone --assume-yes-for-downloads --onefile --windows-icon-from-ico=img\icon.ico .\console.py
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Console build successful! Output as console.exe"
 } else {
