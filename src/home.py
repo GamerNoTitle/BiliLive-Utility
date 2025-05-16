5beda6762a7b2566692d76b64bf49d377aafbb8f
GamerNoTitle <GamerNoTitle@outlook.com>
2025-05-17 01:28:09 +0800

    feat: 如果用户未在开播前获取信息，则自动获取一次

--- src/home.py
+++ src/home.py
@@ -608,9 +608,13 @@ def get_main_content(page: ft.Page):
     # 开播按钮点击
     def start_live_clicked(e):
         if prev_tags == [] or prev_title == "":
-            result_text.value = "❌ 你必须先获取你的直播间信息后才能开播！"
-            save_log_to_file("用户在开播前未获取直播间信息", level="ERROR")
-            return 
+            save_log_to_file(
+                "用户在开播前未获取直播间信息，尝试自动获取", level="WARN")
+            get_liveroom_data_clicked(e=None)
+            # result_text.value = "❌ 你必须先获取你的直播间信息后才能开播！"
+            # save_log_to_file("用户在开播前未获取直播间信息", level="ERROR")
+            # page.update()
+            # return 
         result_text.value = ""
         stream_addr_input.value = ""
         stream_code_input.value = ""
