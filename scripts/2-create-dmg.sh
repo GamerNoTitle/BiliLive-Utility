a97358d5d581c561c0b553a6bb654c13bb6d639a
GamerNoTitle <GamerNoTitle@outlook.com>
2026-02-07 18:56:27 +0800

    feat: 更新 dmg 打包脚本，添加背景

--- scripts/2-create-dmg.sh
+++ scripts/2-create-dmg.sh
@@ -1,3 +1,21 @@
 cd dist
-hdiutil create -volname "BiliLive Utility" -srcfolder . -ov -format UDBZ "./BiliLive Utility.dmg"
-rm -rf "BiliLive Utility.app" "BiliLive Utility"
+rm -rf "BiliLive Utility"
+
+# Get version from environment variable if available
+VOLNAME="BiliLive Utility"
+if [ -n "$AG_VERSION" ]; then
+  VOLNAME="BiliLive Utility $AG_VERSION"
+fi
+
+create-dmg \
+  --volname "$VOLNAME" \
+  --window-pos 400 200 \
+  --window-size 660 400 \
+  --icon-size 100 \
+  --icon "BiliLive Utility.app" 160 185 \
+  --hide-extension "BiliLive Utility.app" \
+  --app-drop-link 500 185 \
+  --background "../static/dmg-bg.png" \
+  "BiliLive Utility.dmg" \
+  "."
+rm -rf "BiliLive Utility.app"
